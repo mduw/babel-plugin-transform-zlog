a2a32d4de9db51028e5320b9c504488ee2f25349
minhdlc <minlcdang@gmail.com>
2022-05-19 15:39:26 +0700

    update(turn on global check): check for global id

--- src/utils/verify-identifier.js
+++ src/utils/verify-identifier.js
@@ -1,9 +1,9 @@
 export function isGlobalIdentifier(nodePath, identifier) {
-  return true;
-  // const { globals } = nodePath.scope;
-  // if (!globals) throw new Error('Error: global scope == undefined');
-  // if (Object.prototype.hasOwnProperty.call(globals, identifier)) {
-  //   return true;
-  // }
-  // return false;
+  // return true;
+  const { globals } = nodePath.scope;
+  if (!globals) throw new Error('Error: global scope == undefined');
+  if (Object.prototype.hasOwnProperty.call(globals, identifier)) {
+    return true;
+  }
+  return false;
 }
