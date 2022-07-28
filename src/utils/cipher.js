e9a5ce2a964c12259319c275d38eb5e590b9bc1b
minhdlc <minlcdang@gmail.com>
2022-07-28 14:32:41 +0700

    update cipher

--- src/utils/cipher.js
+++ src/utils/cipher.js
@@ -9,7 +9,7 @@ class Cipher {
 
   encrypt = text => {
     const iv = Buffer.from(this.secrets.iv, 'hex');
-    const cipher = crypto.createCipheriv(this.algorithm, this.secrets.secretKey, iv);
+    const cipher = crypto.createCipheriv(this.algorithm, this.secrets.key, iv);
     const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
     return encrypted.toString('hex');
   };
